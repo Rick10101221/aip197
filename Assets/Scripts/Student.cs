d17b9d047dff7381e4b92f174e705e297e4c038e
axetion <akt@stellarcore.net>
2020-02-04 16:31:18 -0800

    Proper VR pointer

--- Assets/Scripts/Student.cs
+++ Assets/Scripts/Student.cs
@@ -1,33 +1,26 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(LineRenderer))]
 public class Student : MonoBehaviour
 {
     #pragma warning disable CS0649
     [SerializeField]
-    private GameObject pencil, paper;
+    private GameObject paper, pencil, pointer;
 
     /* How hard the pencil can hit the test to automatically leave behind a mark */
     [SerializeField]
     private float impactThreshold = 0.5f;
 
-    /* How much the pencil is tilted up from the pointer. */
+    /* How much the pencil is tilted from the pointer. */
     [SerializeField]
-    private float pencilSlant = 30f;
+    private float pencilSlant = 120f;
     #pragma warning restore CS0649
-
-    private LineRenderer pointer;
-
-    private void Awake()
-    {
-        pointer = GetComponent<LineRenderer>();
-    }
 
     private void LateUpdate()
     {
         pointer.transform.position = pencil.transform.position;
         pointer.transform.rotation = pencil.transform.rotation;
-        pencil.transform.rotation *= Quaternion.AngleAxis(pencilSlant, Vector3.left);
+
+        pencil.GetComponentInChildren<MeshRenderer>().transform.rotation *= Quaternion.AngleAxis(pencilSlant, Vector3.left);
     }
 }
---
+++ Assets/Shaders/Pointer.shader
@@ -0,0 +1,51 @@
+﻿Shader "Custom/Pointer"
+{
+    Properties
+    {
+        _Color ("Color", color) = (1.0, 0.0, 0.0, 1.0)
+    }
+    SubShader
+    {
+        Tags
+        { 
+            "RenderType" = "Transparent"
+            "Queue" = "Transparent"
+        }
+
+        ZTest Off
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            float4 _Color;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return _Color;
+            }
+            ENDCG
+        }
+    }
+}
